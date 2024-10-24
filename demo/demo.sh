c79a30cd9667106575ef73ef7584f9d1e137959a
wscalf <wscalf@gmail.com>
2024-10-24 17:02:55 -0400

    Fix demo to not use old ksl (#11)

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,3 +1,4 @@
+KSL=../bin/ksl
 echo "KSL modules encapsulate the access management requirements for a subdomain (ex: a service)"
 read -n 1 -s -r -p "Press any key to continue..."
 echo
@@ -10,9 +11,9 @@ echo
 echo "KSL files can be built directly into schemas for backend Zanzibar implementations."
 read -n 1 -s -r -p "Press any key to continue..."
 echo
-echo ./ksl -o schema.zed rbac.ksl
+echo $KSL -o schema.zed rbac.ksl
 sleep 1
-./ksl -o schema.zed rbac.ksl
+$KSL -o schema.zed rbac.ksl
 echo cat schema.zed
 sleep 1
 cat schema.zed
@@ -22,9 +23,9 @@ echo
 echo "KSL files can also be compiled to an intermediate language"
 read -n 1 -s -r -p "Press any key to continue..."
 echo
-echo ./ksl -c rbac.ksl -o rbac.json
+echo $KSL -c rbac.ksl -o rbac.json
 sleep 1
-./ksl -c rbac.ksl -o rbac.json
+$KSL -c rbac.ksl -o rbac.json
 cat rbac.json
 sleep 5
 echo
@@ -48,9 +49,9 @@ echo
 echo "And we can generate a schema from any combination of ksl and compiled json files"
 read -n 1 -s -r -p "Press any key to continue..."
 echo
-echo ./ksl -o combined.zed rbac.ksl typespec/inventory.json
+echo $KSL -o combined.zed rbac.ksl typespec/inventory.json
 sleep 1
-./ksl -o combined.zed rbac.ksl typespec/inventory.json
+$KSL -o combined.zed rbac.ksl typespec/inventory.json
 cat combined.zed
 echo
 echo "Note the generated relations - the typespec code was able to call the extension from the ksl code!"
