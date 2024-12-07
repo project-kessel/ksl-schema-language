daa1e1be8914548161eca96d948c9c73a962678e
wscalf <wscalf@gmail.com>
2024-12-06 21:26:27 -0500

    Initial empty language server for debugging and exploration

--- go.mod
+++ go.mod
@@ -8,6 +8,24 @@ require (
 )
 
 require (
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/iancoleman/strcase v0.3.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/sourcegraph/jsonrpc2 v0.2.0 // indirect
+	github.com/tliron/commonlog v0.2.8 // indirect
+	github.com/tliron/kutil v0.3.11 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+)
+
+require (
 	github.com/authzed/authzed-go v0.11.2-0.20240507202708-8b150c491e4a // indirect
 	github.com/authzed/cel-go v0.20.2 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -25,6 +43,7 @@ require (
 	github.com/rs/zerolog v1.32.0 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/tliron/glsp v0.2.2
 	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
---
+++ internal/ksl-lsp/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"github.com/tliron/commonlog"
+	"github.com/tliron/glsp"
+	protocol "github.com/tliron/glsp/protocol_3_16"
+	"github.com/tliron/glsp/server"
+
+	// Must include a backend implementation
+	// See CommonLog for other options: https://github.com/tliron/commonlog
+	_ "github.com/tliron/commonlog/simple"
+)
+
+const lsName = "KSL"
+
+var (
+	version string = "0.0.1"
+	handler protocol.Handler
+)
+
+func main() {
+	// This increases logging verbosity (optional)
+	commonlog.Configure(1, nil)
+
+	handler = protocol.Handler{
+		Initialize:             initialize,
+		Initialized:            initialized,
+		Shutdown:               shutdown,
+		SetTrace:               setTrace,
+		TextDocumentCompletion: textDocumentCompletion,
+	}
+
+	server := server.NewServer(&handler, lsName, false)
+
+	server.RunStdio()
+}
+
+func initialize(context *glsp.Context, params *protocol.InitializeParams) (any, error) {
+	capabilities := handler.CreateServerCapabilities()
+
+	return protocol.InitializeResult{
+		Capabilities: capabilities,
+		ServerInfo: &protocol.InitializeResultServerInfo{
+			Name:    lsName,
+			Version: &version,
+		},
+	}, nil
+}
+
+func initialized(context *glsp.Context, params *protocol.InitializedParams) error {
+	commonlog.NewInfoMessage(0, "Initialized!")
+	return nil
+}
+
+func shutdown(context *glsp.Context) error {
+	protocol.SetTraceValue(protocol.TraceValueOff)
+	return nil
+}
+
+func setTrace(context *glsp.Context, params *protocol.SetTraceParams) error {
+	protocol.SetTraceValue(params.Value)
+	return nil
+}
+
+func textDocumentCompletion(context *glsp.Context, params *protocol.CompletionParams) (any, error) {
+	return nil, nil
+}
