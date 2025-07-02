4879f7b8d974ba9da8832f596f463c01e4a92535
Jonathan Marcantonio <jmarcant@redhat.com>
2025-07-02 16:11:59 -0400

    Add some tests
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- pkg/ksl/compiler_test.go
+++ pkg/ksl/compiler_test.go
@@ -35,3 +35,56 @@ public type group {
 	assert.NoError(t, err)
 	assert.Equal(t, "rbac", ns.Name)
 }
+
+func TestCompilerErrorsWithIncorrectStartOfFile(t *testing.T) {
+	_, err := Compile(strings.NewReader(`
+versions 0.1
+namespaced rbac
+public type principal {}
+public type group {
+    relation member: [Any principal or group.membr]
+}`))
+
+	assert.Error(t, err)
+}
+
+func TestCompilerPassesWithVersionRelationship(t *testing.T) {
+	ns, err := Compile(strings.NewReader(`
+version 0.1
+namespace rbac
+public type principal {}
+public type group {
+    relation member: [Any principal or group.membr]
+	relation #version: [Any principal]
+}`))
+
+	assert.NoError(t, err)
+	assert.Equal(t, "rbac", ns.Name)
+}
+
+func TestCompilerFailsWithVersionRelationship(t *testing.T) {
+	_, err := Compile(strings.NewReader(`
+version 0.1
+namespace rbac
+public type principal {}
+public type group {
+    relation member: [Any principal or group.membr]
+	relation version: [Any principal]
+}`))
+
+	assert.Error(t, err)
+}
+
+func TestCompilerPassesWithNamespaceRelationship(t *testing.T) {
+	ns, err := Compile(strings.NewReader(`
+version 0.1
+namespace rbac
+public type principal {}
+public type group {
+    relation member: [Any principal or group.membr]
+	relation #namespace: [Any principal]
+}`))
+
+	assert.NoError(t, err)
+	assert.Equal(t, "rbac", ns.Name)
+}
